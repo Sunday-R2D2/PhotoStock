e6a5333ca2638d91718bed415d8aa723123ec38f
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-08-02 00:36:57 +0600

    stylized items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,10 +4,14 @@ import classes from "./Item.module.css";
 function Item(props) {
   return (
     <div className={classes.Item} data-id={props.id}>
-      <h2>{props.title}</h2>
-      <div>{props.date}</div>
-      <p>{props.callout}</p>
       <img src={props.img} alt="" />
+      <div className={classes.desc}>
+        <h2>{props.title}</h2>
+        <div className={classes.dop_info}>
+          <p>{props.callout}</p>
+          <small>{props.date}</small>
+        </div>
+      </div>
     </div>
   );
 }
