dafd88d08ba64cb33efb3a236e89e6185e82b57b
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-08-03 18:06:01 +0600

    fixed bug in Article.js

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -4,18 +4,27 @@ import { connect } from "react-redux";
 import classes from "./Article.module.css";
 
 function Article(props) {
-  const art = props.articleID;
   // console.log(props.articleID);
 
-  return (
-    <div className={classes.Article}>
+  let article = <article>No Article</article>;
+
+  if (props.articleID) {
+    article = (
       <article>
-        <img src={art.img} alt="" />
-        <h2>{art.title}</h2>
-        <p>{art.description}</p>
+        <img src={props.articleID.img} alt="" />
+        <h2>{props.articleID.title}</h2>
+        <p>{props.articleID.description}</p>
       </article>
-    </div>
-  );
+    );
+  } else {
+    article = (
+      <article>
+        <h2>No Article</h2>
+      </article>
+    );
+  }
+
+  return <div className={classes.Article}>{article}</div>;
 }
 
 const mapStateToProps = state => {
